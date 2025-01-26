a84cdf717262e63270cacb5dae9752f6963f6e71
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-01-27 01:46:20 +0530

    ui improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,34 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useEffect, useRef, useTransition } from "react";
+import { useState, useTransition } from "react";
 import { z } from "zod";
 import { Toaster, toast } from "sonner";
 import { sample } from "./server/action";
 
 export default function Home() {
+  const [inputVisibility, setInputVisibility] = useState(true)
   const [isPending, startTransition] = useTransition();  // to enable loading state
   const formSchema = z.string().email();
-
-  const formRef =  useRef<HTMLFormElement>(null);  // to reset form
-  useEffect(()=>{
-    if(isPending){
-      return
-    }
-    formRef.current?.reset();
-  },[isPending])
 
   async function onSubmit(formData: FormData) {
     const userInput = formData.get('userInput');
     const checkInput = formSchema.safeParse(userInput);
 
-    if (!checkInput.success) {
+    if (checkInput.success) {
+      startTransition(async() => {
+        const response = await sample(checkInput.data);
+        if ( response?.status == "success" ) {
+          setInputVisibility(false)
+          return
+        }
+        toast("Something went wrong, please try again.")
+        return
+      })
+    }
+    else if (checkInput.error){
       toast("Please enter a valid Email");
       return;
     }
-    startTransition(async() => {
-      const result =await sample(checkInput.data);
-      console.log(result, "check your email")
-      return
-    })
-    return
   }
 
   return (
@@ -40,9 +38,7 @@ export default function Home() {
           toastOptions={{
             unstyled: true,
             duration: 2000,
-            // classNames: {
             className: "bg-white/30 font-bold text-white rounded-xl shadow-lg p-4",
-            // },
           }} position="bottom-right" />
       </div>
       <div className="overflow-hidden flex min-h-screen m-2 justify-center place-items-start pt-56">
@@ -51,15 +47,29 @@ export default function Home() {
             <div className="font-bold text-white text-4xl font-mono mb-6">{`Pastpricing Daily`}</div>
             <div className="text-white/60 text-xl font-medium">{`Get the top news of the day, delivered straight to your inbox every morning.`}</div>
           </div>
-        <form ref={formRef} action={onSubmit} className="flex align-middle w-full relative overflow-hidden">
-          <input type="text"  name="userInput" placeholder="Your email address"
-          className="overflow-hidden bg-gradient-to-r from-[#4b494932] via-[#700da698] to-[#d40c0ce5] animate-gradient bg-400 text-white/70 relative w-full bg-inherit border-1 border-white rounded-full pl-8 py-4 pr-28 text-lg font-light placeholder-white/60 outline-none" />
-            <Button
-            disabled={isPending}
-            className={`absolute top-[10%] right-[5px] rounded-full h-[80%] aspect-square text-md text-white bg-white/5 ${isPending ? '' : 'hover:bg-[#d40c0c83]'} z-20`}
-            >
-            Subscribe
-            </Button>
+        <form action={onSubmit} className="flex align-middle w-full relative overflow-hidden">
+        {inputVisibility ? (
+            <>
+              <input type="text" autoComplete="off" name="userInput" placeholder="Your email address"
+                className="overflow-hidden bg-gradient-to-r from-[#4b494932] via-[#700da698] to-[#d40c0ce5] animate-gradient bg-400 text-white/70 relative w-full bg-inherit border-1 border-white rounded-full pl-8 py-4 pr-28 text-lg font-light placeholder-white/60 outline-none" />
+              <Button
+                  disabled={isPending}
+                  className={`absolute top-[10%] right-[5px] rounded-full h-[80%] aspect-square text-md text-white bg-white/5 ${isPending ? '' : 'hover:bg-[#d40c0c83]'} z-20`}
+                >
+                  {isPending ? 'Loading..' : 'Subscribe'}
+              </Button>
+            </> ): (
+            <>
+              <div className="text-white m-auto text-center border p-8 rounded-2xl">
+                <div className="font-bold text-2xl mb-2 text-white/90">Thanks for subscribing!  🎉</div>
+                <div className="m-uto text-lg text-white/90">
+                  We&apos;ve <span className="font-semibold text-white bg-gradient-to-r from-[#4b494932] via-[#700da698] to-[#d40c0ce5] animate-gradient bg-400">  sent you an email</span> to confirm your address.
+                  <br />
+                  If you don&apos;t receive it, please also check your spam folder.
+                </div>
+              </div>
+            </> )
+            }
         </form>
         </div>
       </div>
--- app/server/action.ts
+++ app/server/action.ts
@@ -1,4 +1,6 @@
 "use server";
-export async function sample(email: string) {
-    return email;
+
+export async function sample(email: string): Promise<{status: string}> {
+    await new Promise<number>(resolve => setTimeout(resolve, 500));
+    return {status: "success"};
 }

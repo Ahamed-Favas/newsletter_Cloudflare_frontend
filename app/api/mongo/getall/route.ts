1afa52b697613571ced061e86b941345daf2d452
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-01-29 21:22:59 +0530

    modified api

--- app/api/mongo/getall/route.ts
+++ app/api/mongo/getall/route.ts
@@ -1,23 +1,23 @@
 import { prisma } from "@/prisma/prisma";
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
+import { jwtVerify } from "jose";
 
 async function getUsers() {
     return await prisma.user.findMany();
 }
 
 export async function GET(req: Request) {
+    const token = req.headers.get("Authorization")?.split(" ")[1];
+    if (!token) {
+        return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
+    }
+    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
     try {
-        const token = req.headers.get("Authorization")?.split(" ")[1];
-        if (!token) {
-            return NextResponse.json({ message: "Unauthorized" }, { status: 401 });
-        }
-        jwt.verify(token, process.env.JWT_SECRET as string);
-        console.log("SUCCESS: Connected to protected route");
+        await jwtVerify(token, secret);
         const users = await getUsers();
         return NextResponse.json({ users: users }, { status: 200 });
     } catch (error) {
         console.error(error);
-        return NextResponse.json({ message: "Invalid Token" }, { status: 403 });
+        return NextResponse.json({ message: "Invalid or expired token" }, { status: 403 });
     }
 }

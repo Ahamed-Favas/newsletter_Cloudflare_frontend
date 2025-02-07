4fb661fcfe0bcd119f1bea856921b7094f358d33
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-02-07 13:29:03 +0530

    unsubscribe option

--- app/test/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Page() {
-  return (
-    <div className='text-white'>page</div>
-  )
-}
-
-export default Page
--- app/unsubscribe/[slug]/page.tsx
+++ app/unsubscribe/[slug]/page.tsx
@@ -3,8 +3,7 @@ import Background from "@/components/Background";
 async function UnsubscribePage({ params }: { params: { slug: string } }) {
   const token = params.slug;
 
-  const res = await fetch(`https://newsletter.pastpricing.com/api/mongo/${token}`, { cache: "no-store" });
-  const data = await res.json();
+  await fetch(`https://newsletter.pastpricing.com/api/mongo/${token}`, { cache: "no-store" });
 
   return (
     <div className="min-h-screen flex flex-col items-center justify-center">
@@ -12,27 +11,14 @@ async function UnsubscribePage({ params }: { params: { slug: string } }) {
       <div className="overflow-hidden flex min-h-screen m-2 justify-center place-items-start pt-36 md:pt-56">
         <div className="mx-auto w-full max-w-xl rounded-md p-4">
           <div className="text-center">
-            {data.message ? (
-              <>
-                <div className="font-bold text-2xl text-white/90 mb-2">
-                  😢 It&apos;s sad to say goodbye
-                </div>
-                <div className="m-auto text-lg text-white/90">
-                  You&apos;re successfully unsubscribed from our newsletter.
-                  <br />
-                  Btw, you are always welcome to come again 🤗
-                </div>
-              </>
-            ) : (
-              <>
-                <div className="font-bold text-2xl text-white/90 mb-2">
-                  Error
-                </div>
-                <div className="m-auto text-lg text-white/90">
-                  {data.error || "Something went wrong."}
-                </div>
-              </>
-            )}
+              <div className="font-bold text-2xl text-white/90 mb-2">
+                😢 It&apos;s sad to say goodbye
+              </div>
+              <div className="m-auto text-lg text-white/90">
+                You&apos;re successfully unsubscribed from our newsletter.
+                <br />
+                Btw, you are always welcome to come again 🤗
+              </div>
           </div>
         </div>
       </div>

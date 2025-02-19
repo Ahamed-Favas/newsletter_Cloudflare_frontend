4c0fe4b1e5b994a4b8bdfddc76ca3b1aa952deab
Ahamed Favas <ahamedfavasedanatt@gmail.com>
2025-02-19 17:36:13 +0530

    ui improvements

--- components/Background.tsx
+++ components/Background.tsx
@@ -1,19 +1,19 @@
 "use client"
 import Image from 'next/image'
-import ellipse from '@/public/images/Ellipse.png'
+import ellipse2 from '@/public/images/Ellipse2.png'
  
 export default function Background() {
     return (
         <div className="fixed inset-0 -z-10">
             <Image
                 alt="ellipse"
-                src={ellipse}
+                src={ellipse2}
                 placeholder="blur"
                 quality={100}
                 sizes="100vw"
                 fill
                 className="opacity-0 transition-opacity duration-1000 ease-linear"
-                onLoad={(e) => (e.target as HTMLElement).classList.replace('opacity-0', 'opacity-40')}
+                onLoad={(e) => (e.target as HTMLElement).classList.replace('opacity-0', 'opacity-30')}
                 style={{
                     objectFit: 'cover',
                 }}
